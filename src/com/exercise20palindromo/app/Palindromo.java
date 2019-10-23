627ebce25a442c6acc0d1000c4d183f4ac754546
JorgeInfante1 <jorge_infante90@hotmail.com>
2019-10-23 12:32:45 -0500

    Palindome with Stack

---
+++ src/com/exercise20palindromo/app/Palindromo.java
@@ -0,0 +1,20 @@
+package com.exercise20palindromo.app;
+
+import com.exercise20palindromo.model.StringApplications;
+public class Palindromo 
+{
+    public static void main(String[] args) 
+	{
+		String cadPalindrome1 ="Anita lava la tina.";
+		String cadPalindrome2 ="Yo dono rosas, oro no doy!!!!";
+		String cadPalindrome3 = "Isaac no ronca asi";
+		
+		System.out.println(StringApplications.isPalindrome(cadPalindrome1));
+		System.out.println(StringApplications.isPalindrome(cadPalindrome2));
+		System.out.println(StringApplications.isPalindrome(cadPalindrome3));
+		System.out.println(StringApplications.isPalindromeStack(cadPalindrome1));
+		System.out.println(StringApplications.isPalindromeStack(cadPalindrome2));
+		System.out.println(StringApplications.isPalindromeStack(cadPalindrome3));
+	}
+
+}
